1b37e841a7e3140a0d65f58f1e23df31d59f80c5
CezaryJanicki <cezary.janicki@gmail.com>
2023-11-22 15:15:10 +0100

    Add Orders

---
+++ src/orders/dtos/create-order.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateOrderDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(5, 50)
+  client: string;
+
+  @IsNotEmpty()
+  productId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  address: string;
+}
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -10,6 +10,8 @@ import {
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { NotFoundException } from '@nestjs/common';
+import { UpdateOrderDTO } from './dtos/update-order.dto';
+import { CreateOrderDTO } from './dtos/create-order.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -21,29 +23,34 @@ export class OrdersController {
   }
 
   @Get('/:id')
-  getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    const order = this.ordersService.getById(id);
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const order = await this.ordersService.getById(id);
     if (!order) throw new NotFoundException('Order not found');
     return order;
   }
+
   @Delete('/:id')
-  deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
-    if (!this.ordersService.getById(id))
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.ordersService.getById(id)))
       throw new NotFoundException('Order not found');
-    this.ordersService.deleteById(id);
+    await this.ordersService.deleteById(id);
     return { success: true };
   }
 
   @Post('/')
-  create(@Body() orderData) {
+  create(@Body() orderData: CreateOrderDTO) {
     return this.ordersService.create(orderData);
   }
 
   @Put('/:id')
-  update(@Param('id', new ParseUUIDPipe()) id: string, @Body() orderData) {
-    if (!this.ordersService.getById(id))
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() productData: UpdateOrderDTO,
+  ) {
+    if (!(await this.ordersService.getById(id)))
       throw new NotFoundException('Order not found');
-    this.ordersService.updateById(id, orderData);
+
+    await this.ordersService.updateById(id, productData);
     return { success: true };
   }
 }
